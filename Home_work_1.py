c71cb9c3e8611a02677780458ec8257555d8db59
Vladislav Biryulya <redgas2701@gmail.com>
2020-07-13 00:12:08 +0300

    обновление дз

--- Home_work_1.py
+++ Home_work_1.py
@@ -1,32 +1,13 @@
-#!/usr/bin/env python
-
-# coding: utf-8
-
-# In[55]:
-
-
-import re 
+import re
+import collections
 
 pattern = '.*/[0-9]{8}'
 prog = re.compile(pattern)
-
-
-# In[56]:
-
-
 new_list = []
-import collections
-with open ('URLs.txt' , 'r') as str:
+with open('URLs.txt', 'r') as str:
     for line in str:
         line = line.strip()
         if prog.match(line):
             line = line.split('/')
             new_list.append(line[1])
-print (collections.Counter(new_list))      
-
-
-# In[ ]:
-
-
-
-
+print(collections.Counter(new_list))
